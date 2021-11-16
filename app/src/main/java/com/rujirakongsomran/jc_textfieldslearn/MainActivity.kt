d9121033bfc04eb0fa8cc92b609e7aed188624c8
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-11-16 19:43:13 +0700

    Basic TextField

--- app/src/main/java/com/rujirakongsomran/jc_textfieldslearn/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_textfieldslearn/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -16,9 +19,11 @@ import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_textfieldslearn.ui.theme.JC_TextFieldsLearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,32 +51,35 @@ fun Greeting() {
         var text by remember {
             mutableStateOf("Type here...")
         }
-        OutlinedTextField(
+        BasicTextField(
+            modifier = Modifier
+                .background(Color.LightGray)
+                .padding(16.dp),
             value = text,
             onValueChange = {
                 text = it
             },
-            label = {
-                Text(text = "Title")
-            },
-            leadingIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Filled.Email,
-                        contentDescription = null
-                    )
-                }
-            },
-            trailingIcon = {
-                IconButton(onClick = {
-                    Log.d("Trailing Icon", "Clicked")
-                }) {
-                    Icon(
-                        imageVector = Icons.Filled.Check,
-                        contentDescription = null
-                    )
-                }
-            },
+//            label = {
+//                Text(text = "Title")
+//            },
+//            leadingIcon = {
+//                IconButton(onClick = { /*TODO*/ }) {
+//                    Icon(
+//                        imageVector = Icons.Filled.Email,
+//                        contentDescription = null
+//                    )
+//                }
+//            },
+//            trailingIcon = {
+//                IconButton(onClick = {
+//                    Log.d("Trailing Icon", "Clicked")
+//                }) {
+//                    Icon(
+//                        imageVector = Icons.Filled.Check,
+//                        contentDescription = null
+//                    )
+//                }
+//            },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Email,
                 imeAction = ImeAction.Search
