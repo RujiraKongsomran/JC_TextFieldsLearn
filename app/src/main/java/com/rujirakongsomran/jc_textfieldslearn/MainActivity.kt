a5ab5c210fd13dd226d2bd93293f08e869bfab77
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-11-18 16:39:36 +0700

    Create Login Screen

--- app/src/main/java/com/rujirakongsomran/jc_textfieldslearn/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_textfieldslearn/MainActivity.kt
@@ -33,10 +33,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JC_TextFieldsLearnTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    LoginUI()
-                }
+
             }
         }
     }
@@ -142,127 +139,10 @@ fun PasswordTextField() {
     }
 }
 
-@Composable
-fun LoginUI() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(40.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        var username by remember {
-            mutableStateOf("")
-        }
-        var password by rememberSaveable {
-            mutableStateOf("")
-        }
-
-        var passwordVisibility by remember {
-            mutableStateOf(false)
-        }
-
-        val isFormValid by derivedStateOf {
-            username.isNotBlank() && password.length >= 6
-        }
-
-        val icon = if (passwordVisibility)
-            painterResource(id = R.drawable.design_ic_visibility)
-        else
-            painterResource(id = R.drawable.design_ic_visibility_off)
-
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = username,
-            onValueChange = {
-                username = it
-            },
-            label = {
-                Text(text = "Username")
-            },
-            singleLine = true,
-            leadingIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Filled.Person,
-                        contentDescription = "Username Icon"
-                    )
-                }
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-            ),
-            trailingIcon = {
-                if (username.isNotBlank()) {
-                    IconButton(onClick = { username = "" }) {
-                        Icon(
-                            imageVector = Icons.Filled.Clear,
-                            contentDescription = "Clear Icon"
-                        )
-                    }
-                }
-            }
-        )
-
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = password,
-            onValueChange = {
-                password = it
-            },
-            placeholder = {
-                Text(text = "Password")
-            },
-            label = {
-                Text(text = "Password")
-            },
-            leadingIcon = {
-                IconButton(onClick = { }) {
-                    Icon(
-                        imageVector = Icons.Filled.Lock,
-                        contentDescription = "Password Icon"
-                    )
-                }
-            },
-            trailingIcon = {
-                IconButton(onClick = {
-                    passwordVisibility = !passwordVisibility
-                }) {
-                    Icon(
-                        painter = icon,
-                        contentDescription = "Visibility Icon"
-                    )
-                }
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password
-            ),
-            visualTransformation = if (passwordVisibility) VisualTransformation.None
-            else PasswordVisualTransformation()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = { /* ... */ },
-            // Uses ButtonDefaults.ContentPadding by default
-            contentPadding = PaddingValues(
-                start = 20.dp,
-                top = 16.dp,
-                end = 20.dp,
-                bottom = 16.dp
-            ),
-            modifier = Modifier.fillMaxWidth(),
-            enabled = isFormValid
-        ) {
-            Text("Login")
-        }
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JC_TextFieldsLearnTheme {
-        LoginUI()
+
     }
 }
